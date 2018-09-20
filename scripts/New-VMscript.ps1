83ae951be5d92af70b85ebcccddeaa10bb25f19a
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-19 23:10:45 -0700

    Update New-VMscript.ps1

--- scripts/New-VMscript.ps1
+++ scripts/New-VMscript.ps1
@@ -1,8 +1,24 @@
-$ausername="srinivas.gudibanda@outlook.com"
-$apassword="sRi342143" | ConvertTo-SecureString -asPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($ausername,$apassword)
-$login=Login-AzureRmAccount -Credential $credential
-$subscription=Select-AzureRmSubscription -Subscription "RDMI Partner"
-$rg=New-AzureRmResourceGroup -Name "testrg001" -Location "CentralUS"
-[pscustomobject]$rgname=$rg
-#$rgname
+
+workflow Get-AzureVMTutorial
+{
+    #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
+    $CredentialAssetName = 'DefaultAzureCredential'
+
+    #Get the credential with the above name from the Automation Asset store
+    $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+    if(!$Cred) {
+        Throw "Could not find an Automation Credential Asset named '${CredentialAssetName}'. Make sure you have created one in this Automation Account."
+    }
+
+    #Connect to your Azure Account
+    $Account = Add-AzureAccount -Credential $Cred
+    if(!$Account) {
+        Throw "Could not authenticate to Azure using the credential asset '${CredentialAssetName}'. Make sure the user name and password are correct."
+    }
+
+    #TODO (optional): pick the right subscription to use. Without this line, the default subscription for your Azure Account will be used.
+    #Select-AzureSubscription -SubscriptionName "TODO: your Azure subscription name here"
+    
+    #Get all the VMs you have in your Azure subscription
+    $VMs = New-AzureRmResourceGroup -Name "testrg001" -Location "CentralUS"
+

9ff5eadf71841d9a4fceb5bb0fcc676f41b20e6e
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-19 22:21:44 -0700

    Update New-VMscript.ps1

--- scripts/New-VMscript.ps1
+++ scripts/New-VMscript.ps1
@@ -1,3 +1,3 @@
-     Login-azurermaccount
+     Login-AzureRmAccount
      #Install-WindowsFeature -name Web-Server -IncludeManagementTools
     New-AzureRmResourceGroup -Name "testrg" -Location "centralus" -Force

65390e89b576e02e4e1f8b8fb8529fe544d94552
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2018-09-19 22:38:32 -0700

    Update New-VMscript.ps1

--- scripts/New-VMscript.ps1
+++ scripts/New-VMscript.ps1
@@ -1,3 +1,8 @@
-     Login-AzureRmAccount
-     #Install-WindowsFeature -name Web-Server -IncludeManagementTools
-    New-AzureRmResourceGroup -Name "testrg" -Location "centralus" -Force
+$ausername="srinivas.gudibanda@outlook.com"
+$apassword="sRi342143" | ConvertTo-SecureString -asPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($ausername,$apassword)
+$login=Login-AzureRmAccount -Credential $credential
+$subscription=Select-AzureRmSubscription -Subscription "RDMI Partner"
+$rg=New-AzureRmResourceGroup -Name "testrg001" -Location "CentralUS"
+[pscustomobject]$rgname=$rg
+#$rgname
